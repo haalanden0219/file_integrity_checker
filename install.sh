e9e294d949869f4069a6e81628011ce6fb2da703
Evan Haaland <evan.haaland@proton.me>
2025-03-08 21:13:32 -0500

    added a ton to install script, still needs updates

--- install.sh
+++ install.sh
@@ -2,36 +2,54 @@
 
 # Defining files/directories
 SERVICE_NAME="file_integrity_checker"
+SERVICE_FILE="$SERVICE_NAME.service"
+LOGROTATE_FILE="$SERVICE_NAME.logrotate"
 SERVICE_PATH="/etc/systemd/system/filechecker/"
+LOGROTATE_PATH="/etc/logrotate.d"
+LOGFILE_NAME="file_integrity_checker.log"
+LOGFILE_PATH="/var/log/" 
 SERVICE_GROUP="fileman_group"
 SERVICE_USER="fileman"
 INSTALL_DIR="/opt/filechecker/"
-
+GIT_REPO_URL="https://github.com/haalanden0219/file_integrity_checker.git"
 
 # Creating Directories and permsissions
 mkdir -p "$INSTALL_DIR"
 mkdir -p "$SERVICE_PATH"
-#add ownership and groups
-#add ownership and groups
-#add permissions - Install Dir
-#add permissions - service path
 
-# Create logging files
-touch "$INSTALL_DIR/baseline.hash"
-touch "$INSTALL_DIR/paths.conf"
-touch "$INSTALL_DIR/updated.hash"
+# Clone git repo
+cd "$INSTALL_DIR"
+git clone "$GIT_REPO_URL"
 
+# Create user files
+touch "$INSTALL_DIR/baseline.hash" "$INSTALL_DIR/paths.conf" "$INSTALL_DIR/updated.hash"
 
-# Copy files
-#cp file_integrity_checker.service /etc/systemd/system/
-# Set up permissions
+#Create logging file
+touch "$LOGFILE_PATH/$LOGFILE_NAME"
 
 # Set up groups - need to set up verifications
-groupadd fileman_group
-useradd -m -d /opt/filechecker fileman
-passwd fileman
-usermod -g fileman_group fileman
+groupadd "$SERVICE_GROUP"
+useradd -m -d "$INSTALL_DIR" "$SERVICE_USER"
+passwd "$SERVICE_USER"
+usermod -g "$SERVICE_GROUP" "$SERVICE_USER"
 
+# mv log and service file
+mv "$INSTALL_DIR/$SERVICE_FILE" "$SERVICE_PATH"
+chown root:root "$SERVICE_PATH/$SERVICE_FILE"
+
+mv "$INSTALL_DIR/$LOGROTATE_FILE" "$LOGROTATE_PATH"
+chown root:root "$LOGROTATE_PATH/$LOGROTATE_FILE"
+
+# Set up Ownership
+chown "$SERVICE_USER":"$SERVICE_GROUP" "$INSTALL_DIR"
+chown "$SERVICE_USER":"$SERVICE_GROUP" "$INSTALL_DIR/baseline.hash" "$INSTALL_DIR/paths.conf" "$INSTALL_DIR/updated.hash"
+
+# Set up permissions
+chmod 644 "$INSTALL_DIR/paths.conf"
+chmod 600 "$INSTALL_DIR/updated.hash"
+chmod 600 "$INSTALL_DIR/baseline.hash"
 
 # Start the service
-
+systemctl daemon-reload
+systemctl enable filechecker
+systemctl start filechecker
